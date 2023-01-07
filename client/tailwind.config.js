250dd58dcaed414f0dcb40d98b79d7e1cc0945ce
Souvik Nayak <60834400+Souvik-Nayak@users.noreply.github.com>
2023-01-07 15:36:17 +0530

    Revert "login-auth initial changes"

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        br: {
+          50: "#d9f7ff",
+          100: "#ade6ff",
+          200: "#7fdafb",
+          300: "#50d1f8",
+          400: "#24cbf5",
+          500: "#0ab9db",
+          600: "#0085ab",
+          700: "#00577c",
+          800: "#00304c",
+          900: "#00101c",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
